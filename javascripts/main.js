3bdd03fdf9d1612b21c7b76b44539d4dfdfb701c
Nicolas Lanquetin <nicolas.lanquetin@accenture.com>
2013-02-12 14:05:47 +0100

    Automatic Commit via Command Line

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,27 @@
 gadgets.util.registerOnLoadHandler(function() {
   // add code that should run on page load here
+	osapi.people.getViewer().execute(function(viewerData) {
+        if (!viewerData.error) {
+           var viewerDiv = document.getElementById('current_user_id'),
+               viewerThumbnailImg = document.getElementById('viewerThumbnailImg');
+           viewerDiv.innerHTML += viewerData.displayName;
+           viewerThumbnailImg.attributes.getNamedItem("src").nodeValue=viewerData.thumbnailUrl;
+           gadgets.window.adjustHeight();
+        };
+     });
 
+     osapi.people.getViewerFriends().execute(function(viewerFriends) {
+        if (!viewerFriends.error){
+         var resultDiv = document.getElementById('result');
+         resultDiv.innerHTML = viewerFriends.list.length;
+        }
+     });
+
+     var workLocationSpan = document.getElementById('workLocationSpan'),
+         prefs = new gadgets.Prefs(),
+         yourWorkLocation = prefs.getString("work_location");
+         workLocationSpan.innerHTML = yourWorkLocation;
+	
   // resize app window to fit content
   // gadgets.window.adjustHeight();
 });
