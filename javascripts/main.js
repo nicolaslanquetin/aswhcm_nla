8cf33347e83e79e696110cef5b9a1aa8b8c676e2
Nicolas Lanquetin <nicolas.lanquetin@accenture.com>
2013-02-12 18:07:57 +0100

    Automatic Commit via Command Line

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,6 +15,12 @@
         resultDiv.innerHTML = viewerFriends.list.length;
        }
     });
+    
+    getViewerBlog(function(response) {
+        var blog = response.viewerBlog;
+        var blogName = document.getElementById('blog_name');
+        blogName.innerHTML = 'Blog: ' + blog.name
+      });
 
     var workLocationSpan = document.getElementById('workLocationSpan'),
         prefs = new gadgets.Prefs(),
@@ -24,17 +30,50 @@
   }
 
   function postActivity(){
-    var params = {activity: {title:"Hello Jive!",
-                          body:"Posting to the stream is easy!"}
-              };
-     osapi.activities.create(params).execute(myCallback);
+    var params = {activity: {title:"Hello Jive!", body:"Posting to the stream is easy!"}};
+     osapi.activities.create(params).execute(postActivityCallback);
   }
 
-  function myCallback(){
+  function postActivityCallback(){
     var msg = new gadgets.MiniMessage(__MODULE_ID__);
-        msg.createDismissibleMessage("You just posted an activity!");
-        gadgets.window.adjustHeight();
+	msg.createDismissibleMessage("You just posted an activity!");
+	gadgets.window.adjustHeight();
   }
+  
+  function getViewerBlog(callback) {
+      osapi.jive.core.users.get({id: '@viewer'}).execute(function(userResponse) {
+        if(!userResponse.error) {
+          var user = userResponse.data;
+          user.blog.get().execute(function(blogResponse) {
+            if(!blogResponse.error) {
+              var blog = blogResponse.data;
+              callback({viewerBlog: blog});
+            } else {
+              alert(blogResponse.error.message);
+            }
+          });
+        }
+      });
+    }
+
+  postBlogPost = function() {
+    getViewerBlog(function(response) {
+      var blog = response.viewerBlog;
+      var subject = document.getElementById('blog_subject').value;
+      var html = document.getElementById('blog_content').value;
+      var blogEntry = {subject: subject, html: html};
+      var blogPostRequest = blog.posts.create(blogEntry);
+      blogPostRequest.execute(function(blogpostResponse) {
+        if(!blogpostResponse.error) {
+          alert("Yay! Posted.");
+        } else {
+          alert(blogpostResponse.error.message);
+        }
+      });
+    });
+    return false;
+  };
+
 
   gadgets.util.registerOnLoadHandler(init);
   
